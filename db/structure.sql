f3c0ba61c12a766eb3f1479a8bc8b521689f4807
Leonid Ulitsky <ulitsky@gmail.com>
2012-06-30 15:45:22 +0200

    demo

---
+++ db/structure.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
+CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "email" varchar(255), "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
+INSERT INTO schema_migrations (version) VALUES ('20120627222018');
